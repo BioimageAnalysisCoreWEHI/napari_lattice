a867d0941018ba89bc3e2a604f2908ccd8d1d1b8
PRADEEP\Pradeep <pradeep.rajasekhar@gmail.com>
2021-11-08 18:07:21 +1100

    Updated UI to remember last opened folder; Last saved folder isn't working yet. Added flag to disable GPU and use dask for processing. When using dask for previewing deskew, added an option to compute the image before adding to napari or it will deskew on the fly everytime the napari slider is changed

--- src/llsz/ui.py
+++ src/llsz/ui.py
@@ -11,27 +11,38 @@ from magicclass import magicclass, click, field
 import numpy as np
 from napari.types import ImageData
 from napari_plugin_engine import napari_hook_implementation
-from napari.utils import progress
+from napari.utils import progress, history
 
 from llsz.array_processing import get_deskew_arr
 from llsz.transformations import deskew_zeiss
 from llsz.io import LatticeData
 import tifffile
 
+
 @magicclass(widget_type="scrollable", name ="LLSZ analysis")
 class LLSZWidget:
     
     @magicclass(widget_type="list", popup_mode="last")#, close_on_run=False
     class llsz_menu:
-        @set_design(background_color="orange", font_family="Consolas")
-        def Open_File(self, path:Path):
+        @set_design(background_color="orange", font_family="Consolas",visible=True)
+        def Open_File(self, path:Path = Path(history.get_open_history()[0])):
             print("Opening", path)
             self.lattice=LatticeData(path, 30.0, "Y")
             self.aics = self.lattice.data
             self.file_name = os.path.splitext(os.path.basename(path))[0]
             self.save_name=os.path.splitext(os.path.basename(path))[0]
+            #settings.NapariSettings().application.open_history.insert(0,path.__str__())
             self.parent_viewer.add_image(self.aics.dask_data)
             self["Open_File"].background_color = "green" 
+            self.dask = False #Use GPU by default
+            #update the napari settings to use the opened file path as last opened path
+            history.update_open_history(path.__str__())
+        
+        @magicgui(labels = False,auto_call=True)
+        def Use_GPU(self, Use_GPU:bool = True):
+            print("Use GPU set to, ", Use_GPU)
+            self.dask = not(Use_GPU)
+            return 
 
         time_deskew = field(int, options={"min": 0,  "step": 1},name = "Time")
         chan_deskew = field(int, options={"min": 0,  "step": 1},name = "Channels")
@@ -39,6 +50,7 @@ class LLSZWidget:
         @magicgui(header=dict(widget_type="Label",label="<h3>Preview Deskew</h3>"), call_button = "Preview")
         def Preview_Deskew(self, header, img_data:ImageData):#, img_layer:ImageData):
             print("Previewing deskewed channel and time")
+            
             #Function for previewing deskew
             #time_deskew = field(int, options={"min": 0, "max": self.lattice.time,"step": 1},name = "Time")
             #chan_deskew = field(int, options={"min": 0, "max": self.lattice.channels, "step": 1},name = "Channels")
@@ -65,10 +77,16 @@ class LLSZWidget:
             deskew_img=get_deskew_arr(self.aics.dask_data, self.lattice.deskew_shape, self.lattice.deskew_vol_shape, time= time_deskew, channel=chan_deskew, scene=0, skew_dir=self.lattice.skew)
             
             #Perform deskewing on the skewed dask array 
-            deskew_full=deskew_zeiss(deskew_img,angle,shear_factor,scaling_factor,self.lattice.deskew_translate_y,reverse=False,dask=False)
+            deskew_full=deskew_zeiss(deskew_img,angle,shear_factor,scaling_factor,self.lattice.deskew_translate_y,reverse=False,dask=self.dask)
 
             #Crop the z slices to get only the deskewed array and not the empty area
             deskew_final=deskew_full[self.lattice.deskew_z_start:self.lattice.deskew_z_end].astype('uint16') 
+
+            #Load whole image into RAM, otherwise will compute image everytime
+            #TODO: is there a better way to preview deskewed image using DASK?
+            if self.dask:
+                print("Using CPU for deskewing")
+                deskew_final = deskew_final.compute()
 
             #Add layer for cropping
             #viewer.add_shapes(shape_type='polygon', edge_width=5,edge_color='white',face_color=[1,1,1,0],name="Cropping BBOX layer")
@@ -81,7 +99,7 @@ class LLSZWidget:
                                         
             #img_name="Deskewed image_c"+str(chan_deskew)+"_t"+str(time_deskew)
             self.parent_viewer.add_image(deskew_final,name="Deskewed image"+suffix_name)
-
+            print("Deskewing complete")
             #return (deskew_full, {"name":"Uncropped data"})
             #(deskew_final, {"name":img_name})
         
@@ -91,7 +109,7 @@ class LLSZWidget:
                    ch_start = dict(label="Channel Start:"),
                    ch_end = dict(label="Channel End:", value =1 ),
                    save_path = dict(mode ='d',label="Directory to save "))
-        def Deskew_Save(self, header, time_start:int, time_end:int, ch_start:int, ch_end:int, save_path:Path):
+        def Deskew_Save(self, header, time_start:int, time_end:int, ch_start:int, ch_end:int, save_path:Path = Path(history.get_save_history()[0])):
             assert time_start>=0, "Time start should be >0"
             assert time_end < self.lattice.time and time_end >0, "Check time entry "
             assert ch_start >= 0, "Channel start should be >0"
@@ -100,6 +118,9 @@ class LLSZWidget:
             print(time_end)
             print(ch_start)
             print(ch_end)
+            
+            
+
             time_range = range(time_start, time_end)
             channel_range = range(ch_start, ch_end)
             angle=self.lattice.angle
@@ -108,7 +129,6 @@ class LLSZWidget:
 
             #Convert path to string
             save_path = save_path.__str__()
-
             #save channel/s for each timepoint. 
             #TODO: Check speed -> Channel and then timepoint or vice versa, which is faster?
             for time_point in progress(time_range):
@@ -117,7 +137,7 @@ class LLSZWidget:
                     deskew_img=get_deskew_arr(self.aics, self.lattice.deskew_shape, self.lattice.deskew_vol_shape, time= time_point, channel=ch, scene=0, skew_dir=self.lattice.skew)
                     
                     #Perform deskewing on the skewed dask array 
-                    deskew_full=deskew_zeiss(deskew_img,angle,shear_factor,scaling_factor,self.lattice.deskew_translate_y,reverse=False,dask=False)
+                    deskew_full=deskew_zeiss(deskew_img,angle,shear_factor,scaling_factor,self.lattice.deskew_translate_y,reverse=False,dask=self.dask)
 
                     #Crop the z slices to get only the deskewed array and not the empty area
                     deskew_final=deskew_full[self.lattice.deskew_z_start:self.lattice.deskew_z_end].astype('uint16')
@@ -128,20 +148,20 @@ class LLSZWidget:
                 images_array=np.swapaxes(images_array,0,1)
                 final_name=save_path+os.sep+"C"+str(time_point)+"T"+str(time_point)+"_"+self.save_name+".ome.tif"
                 tifffile.imwrite(final_name,images_array,metadata={"axes":"ZCYX",'spacing': self.lattice.dy, 'unit': 'um',},dtype='uint16')
-            print(time_start)
+            #print(time_start)
+            history.update_save_history(final_name)
             print("Complete")
             #print(save_path)
 
+#hook for napari to get LLSZ Widget
 @napari_hook_implementation
 def napari_experimental_provide_dock_widget():
     # you can return either a single widget, or a sequence of widgets
     return [(LLSZWidget, {"name" : "LLSZ Widget"} )]
 
 
-""""
-Testing out UI only
-Disable napari hook to just the UI
 
-ui=LLSZWidget()
-ui.show(run=True)
-"""
+#Testing out UI only
+#Disable napari hook and enable the following two lines to just test the UI
+#ui=LLSZWidget()
+#ui.show(run=True)
