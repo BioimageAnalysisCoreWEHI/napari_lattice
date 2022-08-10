ef368540c84e4f04e9d5c37ad8d4acf9da8615d9
PRADEEP\Pradeep <pradeep.rajasekhar@gmail.com>
2022-08-11 08:43:40 +1000

    adding rbc_workflow

--- src/napari_lattice/config.py
+++ src/napari_lattice/config.py
@@ -2,7 +2,3 @@
 #https://docs.python.org/3/faq/programming.html?highlight=global#how-do-i-share-global-variables-across-modules
 channel = 0
 time = 0 
-
-#a list to hold custom variables that can be called across modules 
-#needs testing
-custom_var = []
