9e3378942892e013ade5079de9342f4786a69448
pr4deepr <pradeep.rajasekhar@gmail.com>
2022-12-09 17:36:56 +1100

    using enums for deskew direction and decon choice

--- src/napari_lattice/__init__.py
+++ src/napari_lattice/__init__.py
@@ -2,7 +2,16 @@
 __version__ = "0.0.1"
 
 
+from enum import Enum
 
 
+# Initialize configuration options
+class DeskewDirection(Enum):
+    X = 1
+    Y = 2
 
 
+class DeconvolutionChoice(Enum):
+    cuda_gpu = 1
+    opencl_gpu = 2
+    cpu = 3
