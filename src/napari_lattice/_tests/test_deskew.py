dabb011e7416cc064564bc2b652d38a1eac7ab23
pr4deepr <pradeep.rajasekhar@gmail.com>
2022-12-13 13:26:12 +1100

    update tests to include batch deskew

--- src/napari_lattice/_tests/test_deskew.py
+++ src/napari_lattice/_tests/test_deskew.py
@@ -6,11 +6,12 @@ import numpy as np
 def test_deskew():
 
     raw = np.zeros((5,5,5))
-    raw[2,0,0] = 1
- 
+    raw[2,0,0] = 10
+    
     deskewed = cle.deskew_y(raw,angle_in_degrees=60)
-
+    
+    #np.argwhere(deskewed>0)
     assert deskewed.shape == (4,8,5)
-    assert deskewed[3,2,0] == 1
+    assert deskewed[2,2,0] == 0.5662433505058289
 
     
