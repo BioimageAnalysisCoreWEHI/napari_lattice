7e67956df1fb0392d144fd22852cdff315b43901
pr4deepr <pradeep.rajasekhar@gmail.com>
2022-12-21 22:50:53 +1100

    disabling decon test as no gpu runner

--- src/napari_lattice/_tests/deconvolution.py
+++ src/napari_lattice/_tests/deconvolution.py
@@ -1,5 +1,6 @@
 # Using similar template as Talley Lamberts from pydcudadecon
 # https://github.com/tlambert03/pycudadecon/blob/main/tests/test_decon.py
+# Github runner has no GPU, so cannot test deconvolution. Should run locally.
 
 import numpy.testing as npt
 from skimage.io import imread
@@ -8,12 +9,16 @@ from napari_lattice.llsz_core import pycuda_decon
 from os.path import dirname
 import os
 
+import pytest
+
+
 test_data_dir = os.path.join(dirname(__file__), "data")
 # data directory containing raw, psf and deconvolved data
 ATOL = 0.015
 RTOL = 0.15
 
 
+# @pytest.mark.skip(reason="GPU not available on Github runners. So cannot test deconvolution. Should run locally")
 def test_deconvolution():
 
     data = imread(test_data_dir+"/raw.tif")
