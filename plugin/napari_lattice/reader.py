7bc9d6c1a746ed998b942f729cd387681c083dca
Pradeep Rajasekhar <pradeep.rajasekhar@gmail.com>
2026-01-22 13:37:55 +1100

    add bioio napari reader

--- plugin/napari_lattice/reader.py
+++ plugin/napari_lattice/reader.py
@@ -15,7 +15,7 @@ import numpy as np
 from napari.layers import Image
 from bioio import BioImage
 
-from typing import List, Optional, Tuple, Collection, TYPE_CHECKING, TypedDict
+from typing import Any, List, Optional, Tuple, Collection, TYPE_CHECKING, TypedDict
 
 from bioio import PhysicalPixelSizes
 from lls_core.models.deskew import DefinedPixelSizes
@@ -54,7 +54,7 @@ def lattice_params_from_napari(
     # This is a set of all pixel sizes that we have seen so far
     metadata_pixel_sizes: set[PhysicalPixelSizes] = set()
     save_names = []
-    # The pixel sizes according to the AICS metadata, if any
+    # The pixel sizes according to the BioIO metadata, if any
     final_imgs: list[DataArray] = []
 
     for img in imgs:
@@ -73,23 +73,28 @@ def lattice_params_from_napari(
 
         save_names.append(save_name)
             
-        if 'aicsimage' in img.metadata.keys():
-            img_data_aics: BioImage = img.metadata['aicsimage']
+        if 'bioio_image' in img.metadata:
+            img_data_bioio: BioImage = img.metadata['bioio_image']
+            
             # If the user has not provided pixel sizes, we extract them fro the metadata
             # Only process pixel sizes that are not none
-            if physical_pixel_sizes is None and all(img_data_aics.physical_pixel_sizes):
-                metadata_pixel_sizes.add(img_data_aics.physical_pixel_sizes)
-
-            metadata_order = list(img_data_aics.dims.order)
-            metadata_shape = list(img_data_aics.dims.shape)
-            while len(metadata_order) > len(img.data.shape):
-                logger.info(f"Image metadata implies there are more dimensions ({len(metadata_order)}) than the image actually has ({len(img.data.shape)})")
-                for i, size in enumerate(metadata_shape):
-                    if size not in img.data.shape:
-                        logger.info(f"Excluding the {metadata_order[i]} dimension to reconcile dimension order")
-                        del metadata_order[i]
-                        del metadata_shape[i]
-            calculated_order = metadata_order
+            if physical_pixel_sizes is None and all(img_data_bioio.physical_pixel_sizes):
+                metadata_pixel_sizes.add(img_data_bioio.physical_pixel_sizes)
+            
+            if "dimensions" in img.metadata:
+                calculated_order = img.metadata["dimensions"]
+            else:
+                metadata_order = list(img_data_bioio.dims.order)
+                metadata_shape = list(img_data_bioio.dims.shape)
+                
+                while len(metadata_order) > len(img.data.shape):
+                    logger.info(f"Image metadata implies there are more dimensions ({len(metadata_order)}) than the image actually has ({len(img.data.shape)})")
+                    for i, size in enumerate(metadata_shape):
+                        if size not in img.data.shape:
+                            logger.info(f"Excluding the {metadata_order[i]} dimension to reconcile dimension order")
+                            del metadata_order[i]
+                            del metadata_shape[i]
+                calculated_order = metadata_order
         elif dimension_order is None:
             raise ValueError("Either the Napari image must have dimensional metadata, or a dimension order must be provided")
         else:
@@ -111,7 +116,7 @@ def lattice_params_from_napari(
     return NapariImageParams(save_name=save_names[0], physical_pixel_sizes=final_pixel_size, data=final_img, dims=final_img.shape)
 
 def napari_get_reader(path: list[str] | str):
-    """Check if file ends with h5 and returns reader function if true
+    """Check if file ends with h5 or supported bioio format (czi, tif) and returns reader function if true
     Parameters
     ----------
     path : str or list of str
@@ -125,15 +130,13 @@ def napari_get_reader(path: list[str] | str):
         # if it is a list, we are only going to open first file
         path = path[0]
 
-    tiff_formats = (".tif",".tiff")
-    
     if path.endswith(".h5"):
         return bdv_h5_reader
-    elif path.endswith(tiff_formats):
-        return tiff_reader
-    # if we know we cannot read the file, we immediately return None.
-    else:
-        return None
+    
+    if path.endswith((".czi", ".tif", ".tiff")):
+        return bioio_reader
+    
+    return None
 
 
 def bdv_h5_reader(path):
@@ -191,18 +194,70 @@ def bdv_h5_reader(path):
     layer_type = "image"  # optional, default is "image"
     return [(images, add_kwargs, layer_type)]
 
-def tiff_reader(path: ImageLike) -> List[Tuple[BioImage, dict, str]]:
-    """Take path to tiff image and returns a list of LayerData tuples.
-    Specifying tiff_reader to have better control over tifffile related errors when using BioImage
+def bioio_reader(path: str | list[str]) -> List[Tuple[Any, dict, str]]:
     """
+    Reader for bioio supported files
+    """
+    if isinstance(path, list):
+        path = path[0]
     
     try:
         image = BioImage(path)
     except Exception as e:
-        raise Exception("Error reading TIFF. Try upgrading tifffile library: pip install tifffile --upgrade.") from e
+        if str(path).endswith((".tif", ".tiff")):
+            raise Exception("Error reading TIFF. Try upgrading tifffile library: pip install tifffile --upgrade.") from e
+        raise e
 
-    # optional kwargs for the corresponding viewer.add_* method
-    add_kwargs = {}
+    layer_data = []
 
-    layer_type = "image"  # optional, default is "image"
-    return [(image, add_kwargs, layer_type)]
+    if not image.scenes:
+        scenes = (None,)
+    else:
+        scenes = image.scenes
+
+    for scene in scenes:
+        if scene is not None:
+            image.set_scene(scene)
+
+        # optional kwargs for the corresponding viewer.add_* method
+        add_kwargs = {}
+
+        # Get the dask data
+        data = image.dask_data
+
+        # Get the dimensions
+        dim_order = list(image.dims.order)
+
+        # Set the scale
+        pixel_sizes = image.physical_pixel_sizes
+        scale = [
+            getattr(pixel_sizes, dim, 1.0) or 1.0 for dim in image.dims.order
+        ]
+        
+        # Set the channel axis
+        if "C" in image.dims.order:
+            c_idx = image.dims.order.index("C")
+            add_kwargs["channel_axis"] = c_idx
+            #remove channel idx from scale
+            scale.pop(c_idx)
+            dim_order.pop(c_idx)
+            if image.channel_names:
+                if len(scenes) > 1 and scene is not None:
+                    add_kwargs["name"] = [f"{scene} - {ch}" for ch in image.channel_names]
+                else:
+                    add_kwargs["name"] = image.channel_names
+        else:
+            if len(scenes) > 1 and scene is not None:
+                add_kwargs["name"] = f"{os.path.splitext(os.path.basename(path))[0]} - {scene}"
+            else:
+                add_kwargs["name"] = os.path.splitext(os.path.basename(path))[0]
+
+        # Set the metadata
+        add_kwargs["metadata"] = {"bioio_image": image, "scene": scene, "dimensions": dim_order}
+
+        add_kwargs["scale"] = scale
+    
+        layer_type = "image"  # optional, default is "image"
+        layer_data.append((data, add_kwargs, layer_type))
+
+    return layer_data
